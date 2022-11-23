2ca76539949da4ed963ed8e48330575461c3573f
Merve <83183081+mervecoskunn@users.noreply.github.com>
2022-11-23 13:40:50 +0100

    .

--- To Do App/app.js
+++ To Do App/app.js
@@ -1,44 +1,43 @@
-// Global Degiskenler
-const form = document.querySelector("#addTaskForm");
+//global Selectors
+const form = document.querySelector('#addTaskForm');
+const input = document.querySelector('#txtTaskName');
+const btnDeleteAll = document.getElementById('btnAddNewTask');
+const taskList = document.getElementById('task-list');
 
-const input= document.querySelector("#txtTaskName");
 
-const btnDeleteAll = document.querySelector("#btnDeleteAll");
-const taskList = document.querySelector("#task-list");
-let items;
 
 eventListeners();
 
-function eventListeners(){
+//eventListeners 
+ function eventListeners(){
     // form submit event
-    form.addEventListener("submit",addNewItem);
-    //delete an item
-    //delete all item
-    
+    form.addEventListener('submit', addNewItem);
+
+
 
 }
+
 function addNewItem(e){
     e.preventDefault();
 
     if(input.value===''){
-        alert('please type a task!');
+        alert('Please type a task!');
         return;
-    }
-
-
-    const li = document.createElement('li');
-    li.className='list-group-item list-group-item-secondary';
-    li.appendChild(document.createTextNode(input.value));
     
+    }
+    const li = document.createElement('li');
+    li.className= "list-group-item list -group-item-secondary";
+    li.appendChild(document.createTextNode(input.value));
 
 
-    const a =document.createElement('a');
-    a.classList='delete-item float-end'
+    const a = document.createElement('a');
+    a.classList = 'delete-item float-end';
     a.setAttribute('href','#');
-    a.innerHTML ='<i class ="bi bi-x-circle"></i>'
+    a.innerHTML ='<i class="bi bi-x-circle"></i>'
 
     li.appendChild(a);
     taskList.appendChild(li);
-
+    
 
 }
+
---
+++ Udemy-To Do App/app.js
@@ -0,0 +1,24 @@
+  //select global selectors
+  const form = document.querySelector("#todo-form");
+  const todoInput = document.querySelector("#todo");
+  const todoList =document.querySelector(".list-group");
+  const firstCardBody =document.querySelectorAll(".card-body")[0];
+  const secondCardBody= document.querySelectorAll(".card-body")[1];  
+
+  const filter = document.querySelector("#filter");
+  const clearButton =document.querySelector("#clear-todos");
+
+
+  function eventListeners(){ //for all eventlisteners
+    form.addEventListener("submit",addTodo);
+
+}
+function addTodo(e){
+    const newTodo =todoInput.value;
+
+    console.log(newTodo);
+
+
+    e.preventDefault();
+
+}
